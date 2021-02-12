34fea9fa156b225aae8cc6cdd91e9147d5119cd3
Frederik Seiffert <frederik@algoriddim.com>
2021-02-12 16:11:36 +0100

    Improved formatting.

--- phases/11-libobjc2.bat
+++ phases/11-libobjc2.bat
@@ -8,10 +8,17 @@ set BUILD_DIR="%SRCROOT%\%PROJECT%\build-%ARCH%"
 if not exist %BUILD_DIR% (mkdir %BUILD_DIR%)
 cd "%BUILD_DIR%"
 
+echo.
 echo ### Running cmake
 :: Note: build type must be Release or RelWithDebInfo so we link against the
 :: release CRT DLLs just like all our other projects.
-cmake .. -G Ninja -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX="%INSTALL_PREFIX%" -DCMAKE_C_COMPILER=clang-cl -DCMAKE_CXX_COMPILER=clang-cl || exit /b 1
+cmake .. ^
+  -G Ninja ^
+  -DCMAKE_BUILD_TYPE=RelWithDebInfo ^
+  -DCMAKE_INSTALL_PREFIX="%INSTALL_PREFIX%" ^
+  -DCMAKE_C_COMPILER=clang-cl ^
+  -DCMAKE_CXX_COMPILER=clang-cl ^
+  || exit /b 1
 
 echo.
 echo ### Building
