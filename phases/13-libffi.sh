66474677b9ba25740bb2b893026b8556f312e073
Frederik Seiffert <frederik@algoriddim.com>
2021-02-12 07:33:52 +0100

    Added working libffi phase.

--- phases/13-libffi.sh
+++ phases/13-libffi.sh
@@ -1,27 +1,39 @@
 #!/bin/sh
 set -e
 
-PROJECT=libffi
-REPO=https://github.com/libffi/libffi.git
+export PROJECT=libffi
+export REPO=https://github.com/libffi/libffi.git
 
 `dirname $0`/common.bat prepare_project
 
-exit 0
-
 cd "$SRCROOT/$PROJECT"
 
-echo
-echo "### Running autogen"
-./autogen.sh
+if [ ! -f configure ]; then
+  echo
+  echo "### Running autogen"
+  ./autogen.sh
+  echo
+fi
 
-echo
 echo "### Running configure"
+MSVCC="$PWD/msvcc.sh"
+if [ "$ARCH" == "x86" ]; then
+  MSVCC="$MSVCC -m32"
+  TARGET=i686-pc-cygwin # cygwin suffix required for building DLL
+elif [ "$ARCH" == "x64" ]; then
+  MSVCC="$MSVCC -m64"
+  TARGET=x86_64-pc-cygwin
+else
+  echo Unknown ARCH: $ARCH && exit 1
+fi
+rm -rf $TARGET
 ./configure \
-  --prefix="$INSTALL_PREFIX" \
-  CC="$PWD/msvcc.sh -clang-cl" \
-  CXX="$PWD/msvcc.sh -clang-cl" \
-  LD=link \
-  CPP="clang-cl -EP" 
+  --build=$TARGET --host=$TARGET \
+  --prefix="$UNIX_INSTALL_PREFIX" \
+  --disable-docs \
+  CC="$MSVCC" CXX="$MSVCC" LD=link \
+  CPP="cl -nologo -EP" CXXCPP="cl -nologo -EP" \
+  CPPFLAGS="-DFFI_BUILDING_DLL" LDFLAGS="" \
 
 echo
 echo "### Building"
@@ -29,4 +41,8 @@ make -j`nproc`
 
 echo
 echo "### Installing"
-make install
+# make install throws errors for DLL builds, so we install manually instead
+cd $TARGET
+install -D -t "$UNIX_INSTALL_PREFIX"/include/ include/*.h
+install -D -t "$UNIX_INSTALL_PREFIX"/lib/ .libs/libffi-*.dll
+install .libs/libffi-*.lib "$UNIX_INSTALL_PREFIX"/lib/ffi.lib
