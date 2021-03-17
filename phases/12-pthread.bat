fd66fde01be6d96d9d86ee68b1e5c73b7a47d7fa
Frederik Seiffert <frederik@algoriddim.com>
2021-03-17 16:11:04 +0100

    pthread: add logging and support install prefix on different drive

--- phases/12-pthread.bat
+++ phases/12-pthread.bat
@@ -3,12 +3,13 @@ call "%~dp0\common.bat" loadenv || exit /b 1
 
 set PTHREAD_RELEASE_URL=ftp://sourceware.org/pub/pthreads-win32/prebuilt-dll-2-9-1-release
 
-cd "%INSTALL_PREFIX%" || exit /b 1
+pushd "%INSTALL_PREFIX%" || exit /b 1
 
 if not exist lib mkdir lib || exit /b 1
 pushd lib
 if not exist pthreadVC2.dll (curl -O# %PTHREAD_RELEASE_URL%/dll/%ARCH%/pthreadVC2.dll || exit /b 1)
 if not exist pthreadVC2.lib (curl -O# %PTHREAD_RELEASE_URL%/lib/%ARCH%/pthreadVC2.lib || exit /b 1)
+dir /b /s pthreadVC2.*
 popd
 
 if not exist include mkdir include || exit /b 1
@@ -27,6 +28,9 @@ if not exist pthread.h (
   type pthread.orig.h >> pthread.h
   del pthread.orig.h
 )
+dir /b /s sched.h semaphore.h pthread.h
+popd
+
 popd
 
 echo pthreads-win32 installed
