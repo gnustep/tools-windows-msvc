9ba81e61a97984bd1aecdab8c9f6b0a77254f835
Frederik Seiffert <frederik@algoriddim.com>
2021-10-27 13:10:48 +0200

    WIP: Add support for libs-gui

---
+++ phases/19-libtiff.bat
@@ -0,0 +1,32 @@
+@echo off
+setlocal
+
+set PROJECT=libtiff
+set REPO=https://gitlab.com/libtiff/libtiff.git
+set TAG=
+
+:: load environment and prepare project
+call "%~dp0\..\scripts\common.bat" prepare_project || exit /b 1
+
+set BUILD_DIR="%SRCROOT%\%PROJECT%\build-%ARCH%-%BUILD_TYPE%"
+if exist "%BUILD_DIR%" (rmdir /S /Q "%BUILD_DIR%" || exit /b 1)
+mkdir "%BUILD_DIR%" || exit /b 1
+cd "%BUILD_DIR%" || exit /b 1
+
+echo.
+echo ### Running cmake
+cmake .. %CMAKE_OPTIONS% ^
+  -D cxx=OFF ^
+  || exit /b 1
+
+echo.
+echo ### Building
+ninja || exit /b 1
+
+echo.
+echo ### Installing
+ninja install || exit /b 1
+
+:: rename tiffd.lib to tiff.lib to allow linking using -ltiff
+cd "%INSTALL_PREFIX%\lib"
+if exist tiffd.lib move /y tiffd.lib tiff.lib
---
+++ phases/20-libpng.bat
@@ -0,0 +1,30 @@
+@echo off
+setlocal
+
+set PROJECT=libpng
+set GITHUB_REPO=glennrp/libpng
+set TAG=
+
+:: load environment and prepare project
+call "%~dp0\..\scripts\common.bat" prepare_project || exit /b 1
+
+set BUILD_DIR="%SRCROOT%\%PROJECT%\build-%ARCH%-%BUILD_TYPE%"
+if exist "%BUILD_DIR%" (rmdir /S /Q "%BUILD_DIR%" || exit /b 1)
+mkdir "%BUILD_DIR%" || exit /b 1
+cd "%BUILD_DIR%" || exit /b 1
+
+echo.
+echo ### Running cmake
+cmake .. %CMAKE_OPTIONS% ^
+  -D PNG_STATIC=OFF ^
+  -D PNG_EXECUTABLES=OFF ^
+  -D PNG_TESTS=OFF ^
+  || exit /b 1
+
+echo.
+echo ### Building
+ninja || exit /b 1
+
+echo.
+echo ### Installing
+ninja install || exit /b 1
