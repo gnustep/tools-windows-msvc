afbfcad0149d3703a7e26192819deb550b296adb
Gregory John Casamento <greg.casamento@gmail.com>
2022-05-10 01:15:22 -0400

    Add new batch file to build libfreetype2

--- phases/21-freetype2.bat
+++ phases/21-freetype2.bat
@@ -1,8 +1,8 @@
 @echo off
 setlocal
 
-set PROJECT=cairo
-set GITHUB_REPO=gcasa/cairo
+set PROJECT=freetype2
+set GITHUB_REPO=aseprite/freetype2
 set TAG=
 
 :: load environment and prepare project
@@ -13,16 +13,23 @@ if exist "%BUILD_DIR%" (rmdir /S /Q "%BUILD_DIR%" || exit /b 1)
 mkdir "%BUILD_DIR%" || exit /b 1
 cd "%BUILD_DIR%" || exit /b 1
 
-pwd
-echo
-echo "### Running configure"
-./configure \
-  --host=$TARGET \
+echo.
+echo ### Running cmake
+:: GNUSTEP_CONFIG is set to empty string to prevent CMake from finding it in install root.
+cmake .. %CMAKE_OPTIONS% ^
+  -D GNUSTEP_CONFIG= ^
+  || exit /b 1
 
-echo
-echo "### Building"
-make -j`nproc`
+echo.
+echo ### Building
+set CCC_OVERRIDE_OPTIONS=x-TC x-TP x/TC x/TP
+ninja || exit /b 1
+set CCC_OVERRIDE_OPTIONS=
 
-echo
-echo "### Installing"
-make instal
+echo.
+echo ### Installing
+ninja install || exit /b 1
+
+:: move DLL to bin and install PDB files
+if not exist "%INSTALL_PREFIX%\bin\" mkdir "%INSTALL_PREFIX%\bin\" || exit /b 1
+if exist "%INSTALL_PREFIX%\lib\freetype.dll" move /Y "%INSTALL_PREFIX%\lib\freetype.dll" "%INSTALL_PREFIX%\bin\" || exit /b 1
