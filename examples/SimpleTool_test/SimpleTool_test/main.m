0ee42dc6bfb1910e2b76ec4163e9ba114cf925de
Gregory Casamento <greg.casamento@gmail.com>
2022-06-30 11:30:42 -0400

    Add simpletool as an example.  Add setup_path script

---
+++ examples/SimpleTool_test/SimpleTool_test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SimpleTool_test
+//
+//  Created by Gregory Casamento on 6/30/22.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
