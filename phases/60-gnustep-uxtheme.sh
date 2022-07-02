0e2aed50ff3ef278cc1de57a00feea5e47e4fbcc
Gregory Casamento <greg.casamento@gmail.com>
2022-07-02 12:34:30 -0400

    Fix cr/lf issues

---
+++ phases/60-gnustep-uxtheme.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+set -eo pipefail
+shopt -s inherit_errexit
+
+cd `dirname $0`
+
+export PROJECT=gnustep-uxtheme
+export GITHUB_REPO=gnustep/plugins-themes-WinUXTheme
+export TAG=
+
+# load environment and prepare project
+../scripts/common.bat prepare_project
+
+cd "$SRCROOT/$PROJECT"
+
+echo
+echo "### Loading GNUstep environment"
+. "$UNIX_INSTALL_PREFIX/share/GNUstep/Makefiles/GNUstep.sh"
+
+export LDFLAGS="-L/mingw64/lib"
+
+echo
+echo "### Building"
+make -j`nproc`
+
+echo
+echo "### Installing"
+make install
