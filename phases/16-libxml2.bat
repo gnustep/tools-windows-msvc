df714d4814e26f5c30af4fae2ec2f57ba6f563b1
Hugo Melder <service@hugomelder.com>
2022-08-19 19:53:34 +0200

    Use ninja install

--- phases/16-libxml2.bat
+++ phases/16-libxml2.bat
@@ -21,13 +21,13 @@ cd "%BUILD_DIR%" || exit /b 1
 echo.
 echo ### Running cmake
 cmake .. %CMAKE_OPTIONS% ^
-  -D BUILD_SHARED_LIBS=NO ^
-  -D LIBXML2_WITH_LZMA=NO ^
-  -D LIBXML2_WITH_PYTHON=NO ^
-  -D LIBXML2_WITH_ZLIB=NO ^
-  -D LIBXML2_WITH_TESTS=NO ^
-  -D LIBXML2_WITH_PROGRAMS=NO ^
-  || exit /b 1
+-D BUILD_SHARED_LIBS=NO ^
+-D LIBXML2_WITH_LZMA=NO ^
+-D LIBXML2_WITH_PYTHON=NO ^
+-D LIBXML2_WITH_ZLIB=NO ^
+-D LIBXML2_WITH_TESTS=NO ^
+-D LIBXML2_WITH_PROGRAMS=NO ^
+|| exit /b 1
 
 echo.
 echo ### Building
@@ -35,19 +35,5 @@ ninja || exit /b 1
 
 echo.
 echo ### Installing
-:: rename {libxml2sd.lib,libxml.lib} to xml2.lib to allow linking using -lxml2
-:: (the wildcard suffix is required to suppress the "file or directory" prompt)
-:: 
-:: The "d" suffix is only present when building in debug mode.
-if "%BUILD_TYPE%" == "Release" (
-  xcopy /Y /F "libxml2s.lib" "%INSTALL_PREFIX%\lib\xml2.lib*" || exit /b 1
-) else (
-  xcopy /Y /F "libxml2sd.lib" "%INSTALL_PREFIX%\lib\xml2.lib*" || exit /b 1
-)
-xcopy /Y /F "libxml\xmlversion.h" "%INSTALL_PREFIX%\include\libxml\" || exit /b 1
-xcopy /Y /F "libxml2-config.cmake" "%INSTALL_PREFIX%\share\cmake\Modules\" || exit /b 1
-xcopy /Y /F "libxml2-config-version.cmake" "%INSTALL_PREFIX%\share\cmake\Modules\" || exit /b 1
-xcopy /Y /F "%SRCROOT%\%PROJECT%\include\libxml\*.h" "%INSTALL_PREFIX%\include\libxml\" || exit /b 1
-
-:: write pkgconfig file
-call "%~dp0\..\scripts\common.bat" write_pkgconfig libxml-2.0 %TAG% -DLIBXML_STATIC -lxml2 -liconv -lws2_32 || exit /b 1
+:: libxml2-export.cmake is generated while installing libxml2
+ninja install
