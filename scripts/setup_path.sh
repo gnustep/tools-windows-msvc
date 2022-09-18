7578f3251553af124f03103d6f588caae6723340
Gregory Casamento <greg.casamento@gmail.com>
2022-09-18 07:59:37 -0400

    Update patches, move gui to master

--- scripts/setup_path.sh
+++ scripts/setup_path.sh
@@ -1,1 +1,1 @@
-export PATH="/c/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Tools/Llvm/x64/bin":${PATH}
+export PATH="/c/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/Llvm/x64/bin":${PATH}
