fe7ac486027d9e5699bed76b369e84cc1413cb1c
Gregory Casamento <greg.casamento@gmail.com>
2022-09-29 10:57:21 -0400

    Add scripts to build imagamagick

--- scripts/setup_path.sh
+++ scripts/setup_path.sh
@@ -1,1 +1,1 @@
-export PATH="/c/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/Llvm/x64/bin":${PATH}
+export PATH="/c/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/Llvm/x64/bin":"/c/Program Files/CMake/bin":${PATH}
