9a9765fdac84060a3d476d59478279456c218467
Frederik Seiffert <frederik@algoriddim.com>
2022-09-30 14:06:16 +0200

    gnustep-corebase: add configure option added upstream

--- phases/40-gnustep-corebase.sh
+++ phases/40-gnustep-corebase.sh
@@ -22,6 +22,7 @@ echo
 echo "### Running configure"
 ./configure \
   --host=$TARGET \
+  --disable-cfrunloop \
   `# specify environment since it doesn't use gnustep-config to get these` \
   CC="`gnustep-config --variable=CC`" \
   CPP="`gnustep-config --variable=CPP`" \
