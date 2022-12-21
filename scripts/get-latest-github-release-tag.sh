0c7d8055d175f3e3e24d171e905d822a8722dafb
Frederik Seiffert <frederik@algoriddim.com>
2022-12-21 22:26:15 +0100

    Add error output for curl

--- scripts/get-latest-github-release-tag.sh
+++ scripts/get-latest-github-release-tag.sh
@@ -14,7 +14,7 @@ if [ -z $GITHUB_REPO ]; then
 fi
 
 # get the tags JSON from the GitHub API and parse it manually
-curl -s https://api.github.com/repos/$GITHUB_REPO/tags \
+curl --silent --show-error --fail-with-body https://api.github.com/repos/$GITHUB_REPO/tags \
   | grep '"name":' \
   | sed -E 's/.*"([^"]+)".*/\1/' \
   | egrep "^${TAG_PREFIX:-[a-z_-]+}[0-9]+[\._-][0-9]+([\._-][0-9]+)?\$" \
