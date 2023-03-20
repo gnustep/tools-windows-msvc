c9455501e1c8d87456b893dd98d4b4bb560b40d2
Gregory Casamento <greg.casamento@gmail.com>
2023-03-20 15:47:07 +0000

    Update install_dnssd.sh script

--- install_dnssd.sh
+++ install_dnssd.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+echo "Create dirs..."
+mkdir -p /c/GNUstep/x64/Debug/lib
+mkdir -p /c/GNUstep/x64/Debug/bin
+mkdir -p /c/GNUstep/x64/Release/lib
+mkdir -p /c/GNUstep/x64/Release/bin
+
+echo "Copy library..."
 # Copy mdns stuff into the folders so GNUstep can build with them...
 cp ./dependencies/bin/dnssd.dll /c/GNUstep/x64/Debug/bin
 cp ./dependencies/bin/dnssd.dll /c/GNUstep/x64/Debug/bin/dns_sd.dll
@@ -13,3 +20,5 @@ cp ./dependencies/bin/dnssd.dll /c/GNUstep/x64/Release/bin/dns_sd.dll
 cp ./dependencies/lib/dnssd.lib /c/GNUstep/x64/Release/lib
 cp ./dependencies/lib/dnssd.lib /c/GNUstep/x64/Release/lib/dns_sd.lib
 cp ./dependencies/include/dns_sd.h /c/GNUstep/x64/Release/include
+
+echo "Done."
