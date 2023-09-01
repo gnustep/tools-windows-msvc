13fe3a59200f50939187e264321b5c24d3e942ff
Frederik Seiffert <frederik@algoriddim.com>
2023-09-01 22:40:41 +0200

    Revert "libiconv: clamp to specific commit to support CPUs without AVX"
    This reverts commit 6fd7e34d44629f4f5ff0ddf16652c575dae1a08b.
    
    Issue was resolved upstream.

--- phases/14-libiconv.bat
+++ phases/14-libiconv.bat
@@ -3,8 +3,7 @@ setlocal
 
 set PROJECT=libiconv
 set GITHUB_REPO=kiyolee/libiconv-win-build
-:: clamp to specific commit due to https://github.com/kiyolee/libiconv-win-build/issues/7
-set TAG=a03370a8cd61ffc38b94945aa1926f45cfaf6d40
+set TAG=
 
 :: load environment and prepare project
 call "%~dp0\..\scripts\common.bat" prepare_project || exit /b 1
