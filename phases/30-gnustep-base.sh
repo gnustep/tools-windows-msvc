0acce135b465ad61e7d56a64b3b7c658f03461c6
Hugo Melder <service@hugomelder.com>
2025-07-01 22:32:06 +0200

    fix(gnustep-base): force linking without pkg-config (hack)

--- phases/30-gnustep-base.sh
+++ phases/30-gnustep-base.sh
@@ -6,7 +6,7 @@ cd `dirname $0`
 
 export PROJECT=gnustep-base
 export GITHUB_REPO=gnustep/libs-base
-export TAG=
+export TAG=curl-configure
 
 # load environment and prepare project
 ../scripts/common.bat prepare_project
